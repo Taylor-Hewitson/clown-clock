cd444b5ccf57a47dfea3444b27b80a9f3cdaa606
church0mouse <church0mouse@gmail.com>
2022-06-02 13:03:04 +1000

    Javascript still not telling correct time
    I feel like I'm close to the answer I just need to get these calculations corrected

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-function clockInit(){
-    var date = new Date();
-    var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
-    var clockDivs = [document.getElementById("hour"), document.getElementById("minute"), document.getElementById("second")];
-    
-    var hour = time[1]/2+time[0]*30;
-    
-    clockDivs[0].style.transform="rotate("+ hour +"deg)";
-    clockDivs[1].style.transform="rotate("+ time[1]*6 +"deg)";
-    clockDivs[2].style.transform="rotate("+ time[2]*6 +"deg)";
+function clockInit() {
+  var date = new Date();
+  var time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+  var clockDivs = [
+    document.getElementById("hour"),
+    document.getElementById("minute"),
+    document.getElementById("second"),
+  ];
+
+  var hour = time[0] * 30 + (time[1] * 6) / 12;
+
+  clockDivs[0].style.transform = "rotate(" + hour + "deg)";
+  clockDivs[1].style.transform = "rotate(" + time[1] * 6 + "deg)";
+  clockDivs[2].style.transform = "rotate(" + time[2] * 6 + "deg)";
 }
 
-window.onload = function() {
+window.onload = function () {
+  clockInit();
 
-    clockInit();
-    
-    setInterval(clockInit, 1000);
+  setInterval(clockInit, 1000);
 };
